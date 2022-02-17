70d7fca251b608b96595ccfcdb59c3ce23a9ba50
grinevs <accsergey@gmail.com>
2022-02-17 20:21:25 +0300

    add classes, -enum

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Пришло время практики!");
+        for (Status s : Status.values()) {
+            System.out.println(s.getMessage());
+        }
+
+
     }
 }
---
+++ src/Status.java
@@ -0,0 +1,22 @@
+public enum Status {
+    NEW("new task"),
+    IN_PROGRESS("task in progress"),
+    DONE("task done");
+
+    private final String message;
+
+    Status(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "Status{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
