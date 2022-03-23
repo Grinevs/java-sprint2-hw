4eaef56729cc62a5f77da9b0f0f745f0fa2201ce
Grinevs <accsergey@gmail.com>
2022-03-24 01:37:26 +0300

    tasks add

--- src/model/MyLinkedList.java
+++ src/model/MyLinkedList.java
@@ -1,36 +1,59 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.LinkedList;
-import java.util.NoSuchElementException;
+import java.util.*;
 
-public class MyLinkedList<T, E> {
-    private Node<E> first;
-    private Node<E> last;
+public class MyLinkedList<Task> {
+    private Node<Task> first;
+    private Node<Task> last;
     private int size = 0;
     private List<Task> tasks= new ArrayList<>();
+    private Map<Integer, Node<Task>> indexMap = new HashMap<>();
 
-    void linkLast(E e) {
-        final Node<E> l = last;
-        final Node<E> newNode = new Node<>(l, e, null);
+    public void linkLast(Task e) {
+        final Node<Task> l = last;
+        final Node<Task> newNode = new Node<>(l, e, null);
         last = newNode;
         if (l == null)
             first = newNode;
         else
             l.next = newNode;
         size++;
+        tasks.add(e);
+        indexMap.put(e.getId, last);
     }
 
-    private void linkFirst(E e) {
-        final Node<E> f = first;
-        final Node<E> newNode = new Node<>(null, e, f);
+    public void linkFirst(Task e) {
+        final Node<Task> f = first;
+        final Node<Task> newNode = new Node<>(null, e, f);
         first = newNode;
         if (f == null)
             last = newNode;
         else
             f.prev = newNode;
         size++;
+    }
+
+    public void removeNode(Node<Task> x) {
+        if (tasks.contains(x.item)) {
+            final Node<Task> next = x.next;
+            final Node<Task> prev = x.prev;
+
+            if (prev == null) {
+                first = next;
+            } else {
+                prev.next = next;
+                x.prev = null;
+            }
+
+            if (next == null) {
+                last = prev;
+            } else {
+                next.prev = prev;
+                x.next = null;
+            }
+            x.item = null;
+            size--;
+        }
     }
 
     public int size() {
@@ -41,6 +64,8 @@ public class MyLinkedList<T, E> {
         return tasks;
     }
 
+    public void remove(int id) {
+    }
 }
 
 
--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -1,25 +1,27 @@
 package service;
 
+import model.MyLinkedList;
 import model.Task;
 
+import java.sql.ClientInfoStatus;
 import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private List<Task> history = new ArrayList<>();
+    private MyLinkedList<Task> history = new MyLinkedList<>();
 
     @Override
     public List<Task> getHistory() {
-        return history;
+        return history.getTasks();
     }
 
     @Override
     public void add(Task task) {
         if (history.size() < 10) {
-            history.add(task);
+            history.linkLast(task);
         } else {
             remove(0);
-            history.add(task);
+            history.linkLast(task);
         }
     }
 
